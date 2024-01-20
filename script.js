b3fd60d8c761b03b98ae2043ca9d63c51364600c
Twinkal Narnaware <94154988+twinkleUniverse@users.noreply.github.com>
2024-01-20 19:22:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+	let fac=1
+	for(let i=1;i<=n;i++){
+		fac*=i;
+	}
+	let num=fac;
+	let ans=0;
+	while(num!=0){
+		let digit=num%10;
+		if(digit>0)  break;
+		ans++;
+		num/=10;
+	}
+	return ans;
+
+	
 }
 
 const input = prompt("Enter a number");
